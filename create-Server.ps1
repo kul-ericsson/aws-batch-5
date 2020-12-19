e25296355a03a36873f01e3be24ccb506fe7a469
Kulbhushan <kulbhushan.mayer@>
2020-12-19 11:13:35 +0530

    updated to create Server & then Delete a Server

--- create-Server.ps1
+++ create-Server.ps1
@@ -1,2 +1,21 @@
 ﻿$imageDetails = aws ec2 describe-images --filters Name="tag:Name",Values="ericsson-batch-5-ubuntu-image" | ConvertFrom-Json
-$imageDetails.Images.ImageId
+$amiID = $imageDetails.Images.ImageId
+
+$keyPairs = aws ec2 describe-key-pairs | ConvertFrom-Json
+
+foreach($keyPair in $keyPairs.KeyPairs){
+    if ($keyPair.KeyName -match "ericsson"){
+        $keyName = $keyPair.KeyName
+    }
+}
+
+$serverInfo = aws ec2 run-instances --image $amiID --key-name $KeyName --instance-type "t2.micro" 
+
+write-host "[INFO] Some Deployment"
+Write-Host "[INFO] Some Testing"
+
+sleep 40
+
+foreach($instance in ($serverInfo | ConvertFrom-Json).Instances){
+    aws ec2 terminate-instances --instance-ids $instance.InstanceId
+}
