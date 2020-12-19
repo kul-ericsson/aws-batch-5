20f03c41ed664312cc4c208814ec467e7705a944
Kulbhushan <kulbhushan.mayer@>
2020-12-19 11:24:24 +0530

    updated to delete free volumes

--- create-Server.ps1
+++ create-Server.ps1
@@ -1,4 +1,4 @@
-﻿<#$imageDetails = aws ec2 describe-images --filters Name="tag:Name",Values="ericsson-batch-5-ubuntu-image" | ConvertFrom-Json
+﻿$imageDetails = aws ec2 describe-images --filters Name="tag:Name",Values="ericsson-batch-5-ubuntu-image" | ConvertFrom-Json
 $amiID = $imageDetails.Images.ImageId
 
 $keyPairs = aws ec2 describe-key-pairs | ConvertFrom-Json
@@ -18,8 +18,12 @@ sleep 40
 
 foreach($instance in ($serverInfo | ConvertFrom-Json).Instances){
     aws ec2 terminate-instances --instance-ids $instance.InstanceId
-}#>
+}
 
 foreach($instance in (aws ec2 describe-instances | ConvertFrom-Json).Reservations.Instances){
     aws ec2 terminate-instances --instance-ids $instance.InstanceId
+}
+
+foreach( $vol in (aws ec2 describe-volumes | ConvertFrom-Json).Volumes.VolumeId){
+    aws ec2 delete-volume --volume-id $vol
 }
