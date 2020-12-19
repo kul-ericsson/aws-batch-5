a6781fda5e257f7abbe71885ff00064b54aa4ba2
Kulbhushan <kulbhushan.mayer@>
2020-12-19 11:19:04 +0530

    updated to delete all running instacnes

--- create-Server.ps1
+++ create-Server.ps1
@@ -1,4 +1,4 @@
-﻿$imageDetails = aws ec2 describe-images --filters Name="tag:Name",Values="ericsson-batch-5-ubuntu-image" | ConvertFrom-Json
+﻿<#$imageDetails = aws ec2 describe-images --filters Name="tag:Name",Values="ericsson-batch-5-ubuntu-image" | ConvertFrom-Json
 $amiID = $imageDetails.Images.ImageId
 
 $keyPairs = aws ec2 describe-key-pairs | ConvertFrom-Json
@@ -17,5 +17,9 @@ Write-Host "[INFO] Some Testing"
 sleep 40
 
 foreach($instance in ($serverInfo | ConvertFrom-Json).Instances){
+    aws ec2 terminate-instances --instance-ids $instance.InstanceId
+}#>
+
+foreach($instance in (aws ec2 describe-instances | ConvertFrom-Json).Reservations.Instances){
     aws ec2 terminate-instances --instance-ids $instance.InstanceId
 }
