45e5a9d224c61135edba8bd9bb1ed1f8e25942f8
Kulbhushan <kulbhushan.mayer@>
2020-12-21 15:57:33 +0530

    added MYSQL Provider

--- terraform/create_compute/provider.tf
+++ terraform/create_compute/provider.tf
@@ -1,3 +1,9 @@
 provider "aws" {
   region = "us-east-2"
 }
+
+provider "mysql" {
+  endpoint = output.database_server_enpoint
+  username = output.database_server_username
+  password = output.database_server_password
+}
