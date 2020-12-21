b6a64c95526ea94a21aaf8a2f02fc9763634964b
Kulbhushan <kulbhushan.mayer@>
2020-12-21 10:49:37 +0530

    Added Subnets

---
+++ terraform/vpc/create_subnets.tf
@@ -0,0 +1,27 @@
+resource "aws_subnet" "thinknyx_subnet_1" {
+  vpc_id = aws_vpc.thinknyx_vpc.id
+  cidr_block = "10.10.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-2a"
+  tags = {
+    "Name" = "thinknyx_subnet_1"
+  }
+}
+resource "aws_subnet" "thinknyx_subnet_2" {
+  vpc_id = aws_vpc.thinknyx_vpc.id
+  cidr_block = "10.10.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-2b"
+  tags = {
+    "Name" = "thinknyx_subnet_2"
+  }
+}
+resource "aws_subnet" "thinknyx_subnet_3" {
+  vpc_id = aws_vpc.thinknyx_vpc.id
+  cidr_block = "10.10.3.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-2c"
+  tags = {
+    "Name" = "thinknyx_subnet_3"
+  }
+}
