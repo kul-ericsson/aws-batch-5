603ac096266699e0a60c9d797ddd42f16516a1be
Kulbhushan <kulbhushan.mayer@>
2020-12-21 13:08:40 +0530

    create new EC2 server on Private Subnet

--- terraform/create_compute/create_ec2_server.tf
+++ terraform/create_compute/create_ec2_server.tf
@@ -8,3 +8,14 @@ resource "aws_instance" "thinknyx_ec2_server" {
   subnet_id = data.aws_subnet.thinknyx_public_subnet.id
   vpc_security_group_ids = [ aws_security_group.thinknyx_security_group.id ]
 }
+
+resource "aws_instance" "thinknyx_ec2_server_2" {
+  ami = data.aws_ami.thinknyx_ami.id
+  instance_type = "t2.micro"
+  key_name = "kul-ericsson-thinknyx"
+  tags = {
+    "Name" = "thinknyx_${var.your_name}"
+  }
+  subnet_id = data.aws_subnet.thinknyx_private_subnet.id
+  vpc_security_group_ids = [ aws_security_group.thinknyx_security_group.id ]
+}
