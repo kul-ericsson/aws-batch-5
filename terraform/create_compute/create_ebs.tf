ef2a6d5dfbdaa71ec2a4462d566003b1d9850876
Kulbhushan <kulbhushan.mayer@>
2020-12-21 13:12:41 +0530

    attaching EBS Volume to Server Running in Private Subnet

--- terraform/create_compute/create_ebs.tf
+++ terraform/create_compute/create_ebs.tf
@@ -5,3 +5,10 @@ resource "aws_ebs_volume" "thinknyx_volume" {
     "Name" = "thinknyx_${var.your_name}"
   }
 }
+
+resource "aws_volume_attachment" "thinknyx_volume_attachment" {
+  volume_id = aws_ebs_volume.thinknyx_volume.id
+  instance_id = aws_instance.thinknyx_ec2_server_2.id
+  device_name = "/dev/sdb"
+  skip_destroy = "true"
+}
