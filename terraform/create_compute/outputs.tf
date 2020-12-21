185fbf8cad45afe2e3ef06484e6ae2e4d833a9f9
Kulbhushan <kulbhushan.mayer@>
2020-12-21 14:43:34 +0530

    print resource details on console using Output

---
+++ terraform/create_compute/outputs.tf
@@ -0,0 +1,23 @@
+output "public_ip_of_server_in_public_subnet" {
+  value = aws_instance.thinknyx_ec2_server.public_ip
+}
+
+output "private_ip_of_server_in_private_subnet" {
+  value = aws_instance.thinknyx_ec2_server_2.private_ip
+}
+
+output "database_server_address" {
+  value = aws_db_instance.thinknyx_db_server.address
+}
+
+output "database_server_enpoint" {
+  value = aws_db_instance.thinknyx_db_server.endpoint
+}
+
+output "database_server_username" {
+  value = aws_db_instance.thinknyx_db_server.username
+}
+
+output "database_server_password" {
+  value = aws_db_instance.thinknyx_db_server.password
+}
