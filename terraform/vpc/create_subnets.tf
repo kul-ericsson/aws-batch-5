b3b1d8ef752053f219e1f9a7b558eae22b0bf82e
Kulbhushan <kulbhushan.mayer@>
2020-12-21 13:57:03 +0530

    disbale auto-assign public ip for private subnets

--- terraform/vpc/create_subnets.tf
+++ terraform/vpc/create_subnets.tf
@@ -10,7 +10,7 @@ resource "aws_subnet" "thinknyx_subnet_1" {
 resource "aws_subnet" "thinknyx_subnet_2" {
   vpc_id = aws_vpc.thinknyx_vpc.id
   cidr_block = "10.10.2.0/24"
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
   availability_zone = "us-east-2b"
   tags = {
     "Name" = "thinknyx_subnet_2"
@@ -19,7 +19,7 @@ resource "aws_subnet" "thinknyx_subnet_2" {
 resource "aws_subnet" "thinknyx_subnet_3" {
   vpc_id = aws_vpc.thinknyx_vpc.id
   cidr_block = "10.10.3.0/24"
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
   availability_zone = "us-east-2c"
   tags = {
     "Name" = "thinknyx_subnet_3"
